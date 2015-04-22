783d46c7fa17d691b05f2ab990c8985b18d1fb55
李亚坤 <285577079@qq.com>
2015-04-23 00:00:15 +0800

    no message

--- XinGePush/ViewController.swift
+++ XinGePush/ViewController.swift
@@ -8,9 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
 
-
+    @IBOutlet weak var txtAddItem: UITextField!
+    
+    @IBOutlet weak var tblShoppingList: UITableView!
+    
+    @IBOutlet weak var btnAction: UIButton!
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    var shoppingList: NSMutableArray!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,7 +29,82 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: UITextFieldDelegate method implementation
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if shoppingList == nil {
+            shoppingList = NSMutableArray()
+        }
+        shoppingList.addObject(textField.text)
+        
+        tblShoppingList.reloadData()
+        
+        txtAddItem.text = ""
+        txtAddItem.resignFirstResponder()
+        
+        return true
+    }
 
+    // MARK: UITableViewDelegate method implementation
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        var rows = 0
+        if let list = shoppingList {
+            rows = list.count
+        }
+        return rows
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return CGFloat(50.0)
+    }
+    
+    // MARK: UITableViewDataRource method implementation
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("idCellItem", forIndexPath: indexPath) as! UITableViewCell
+        
+        cell.textLabel!.text = shoppingList[indexPath.row] as? String
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            shoppingList.removeObject(indexPath.row)
+            tblShoppingList.reloadData()
+        }
+    }
+    
+    // MARK: Save to document
+    func saveShoppingList() {
+        let pathsArray = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+        let documentsDirectory = pathsArray[0] as! String
+        let savePath = documentsDirectory.stringByAppendingPathComponent("shopping_list")
+        shoppingList.writeToFile(savePath, atomically: true)
 
+    }
+    
+    // text
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
