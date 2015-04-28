d968d0430b17caa1b16cc150a01fb095ed866751
李亚坤 <285577079@qq.com>
2015-04-28 16:49:38 +0800

    添加了iOS8里面富交互的东西

--- XinGePush/ViewController.swift
+++ XinGePush/ViewController.swift
@@ -23,6 +23,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        datePicker.hidden = true
+        
+        loadShoppingList()
     }
 
     override func didReceiveMemoryWarning() {
@@ -72,21 +75,113 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == UITableViewCellEditingStyle.Delete {
-            shoppingList.removeObject(indexPath.row)
-            tblShoppingList.reloadData()
+            removeItemAtIndex(indexPath.row)
         }
     }
     
-    // MARK: Save to document
+    // MARK: Save/Remove to document
     func saveShoppingList() {
         let pathsArray = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
         let documentsDirectory = pathsArray[0] as! String
         let savePath = documentsDirectory.stringByAppendingPathComponent("shopping_list")
         shoppingList.writeToFile(savePath, atomically: true)
-
     }
     
-    // text
+    func removeItemAtIndex(index: Int)
+    {
+        shoppingList.removeObject(index)
+        tblShoppingList.reloadData()
+        saveShoppingList()
+    }
+    
+    func loadShoppingList() {
+        let pathsArray = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentationDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+        let documentsDirectory = pathsArray[0] as! String
+        let shoppingListPath = documentsDirectory.stringByAppendingPathComponent("Shopping_list")
+        
+        // 判断文件是否存在
+        if NSFileManager.defaultManager().fileExistsAtPath(shoppingListPath) {
+            shoppingList = NSMutableArray(contentsOfFile: shoppingListPath)
+            tblShoppingList.reloadData()
+        }
+    }
+    // MARK: animate Method
+    
+    func animateMyViews(viewToHide: UIView, viewToShow: UIView) {
+        let animationDuration = 0.35
+        
+        UIView.animateWithDuration(animationDuration, animations: { () -> Void in
+            viewToHide.transform = CGAffineTransformScale(viewToHide.transform, 0.001, 0.001)
+            }){ (completion) -> Void in
+                
+                viewToHide.hidden = true
+                viewToShow.hidden = false
+                
+                viewToShow.transform = CGAffineTransformScale(viewToShow.transform, 0.001, 0.001)
+                
+                UIView.animateWithDuration(animationDuration, animations: { () -> Void in
+                    viewToShow.transform = CGAffineTransformIdentity
+                })
+        }
+    }
+    
+    // MARK: UI Method
+    @IBAction func scheduleReminder(sender: AnyObject) {
+        if datePicker.hidden == true {
+            animateMyViews(tblShoppingList, viewToShow: datePicker)
+        }
+        else {
+            animateMyViews(datePicker, viewToShow: tblShoppingList)
+        }
+        txtAddItem.enabled = !txtAddItem.enabled
+    }
+    
+    func setupNotificationSettings() {
+        // Specify the notification types.
+        
+        var notificationTypes: UIUserNotificationType = UIUserNotificationType.Alert | UIUserNotificationType.Sound
+        
+        // UIMutableUserNotification...iOS8 新引入的特性
+        
+        // 点击消失,app仍然在后台，只给几秒钟时间处理任务
+        var justInformation = UIMutableUserNotificationAction()
+        justInformation.identifier = "justInform"
+        justInformation.title = "OK, got it"
+        justInformation.activationMode = UIUserNotificationActivationMode.Background
+        justInformation.destructive = false
+        justInformation.authenticationRequired = false
+        
+        // 点击后添加一个物品
+        var modifyListAction = UIMutableUserNotificationAction()
+        modifyListAction.identifier = "editList"
+        modifyListAction.title = "Edit List"
+        modifyListAction.activationMode = UIUserNotificationActivationMode.Foreground
+        modifyListAction.destructive = false
+        modifyListAction.authenticationRequired = true
+        
+        // 点击后删除
+        var trashAction = UIMutableUserNotificationAction()
+        trashAction.identifier = "trashAction"
+        trashAction.title = "Delete List"
+        trashAction.activationMode = UIUserNotificationActivationMode.Background
+        trashAction.destructive = true
+        trashAction.authenticationRequired = false
+        
+        // 分类
+        let actionsArray = Array(arrayLiteral: justInformation, modifyListAction, trashAction)
+        let actionsArrayMinimal = Array(arrayLiteral: trashAction, modifyListAction)
+        
+        // 创建分类的category
+        var shoppingListReminderCategory = UIMutableUserNotificationCategory()
+        shoppingListReminderCategory.identifier = "ShoppingListReminderCategory"
+        shoppingListReminderCategory.setActions(actionsArray, forContext:UIUserNotificationActionContext.Default)
+        shoppingListReminderCategory.setActions(actionsArrayMinimal, forContext: UIUserNotificationActionContext.Minimal)
+        
+        // 注册设置
+        let categoriesForSettings = Set(arrayLiteral: shoppingListReminderCategory)
+        let newNotificationSettings = UIUserNotificationSettings(forTypes: notificationTypes, categories: categoriesForSettings)
+    }
+    
     
 }
 
